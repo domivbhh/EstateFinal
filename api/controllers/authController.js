944b0a196486edb4beb560cc19c3360d1f2e1279
domivbhh <hariharanvbit123@gmail.com>
2024-04-06 23:22:09 +0530

    created signin backend with cookie sending

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,8 @@
 import { error } from 'console'
 import User from '../models/UserModel.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signUp=async (req,res,next)=>{
     const{username,password,email}=req.body
@@ -19,6 +21,30 @@ export const signUp=async (req,res,next)=>{
 }
 
 
-export const signIn=(req,res)=>{
+export const signIn=async (req,res,next)=>{
+    const {email,password}=req.body
+    try{
+        const validUser=await User.findOne({email});
+
+        if(!validUser){
+            return next(errorHandler(401,'Invalid Credentials'))
+        }
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'Invalid Credentials'))
+        }
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        console.log(token)
+//Nalla note pannu
+        const expiryDate=new Date(Date.now() + 3600000);
+        const sendingData=await User.findOne({email}).select('-password')
+        res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(sendingData)
+
+//mela iruka linea nalla note pannu
+
+    }
+    catch(err){
+        next(err)
+    }
 
 }

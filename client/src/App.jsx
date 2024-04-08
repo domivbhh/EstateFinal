62b8d93dcd92d17bb7e6b5c38da5444203569030
domivbhh <hariharanvbit123@gmail.com>
2024-04-08 07:31:07 +0530

    updated header with profile pic and made profile component has private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
-      <Route path="/profile" element={<Profile/>}/>
+      <Route element={<PrivateRoute/>}>
+        <Route path="/profile" element={<Profile/>}/>
+      </Route>
       <Route path="/signin" element={<Signin/>}/>
       <Route path="/signup" element={<Signup/>}/>
 

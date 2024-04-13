882c5f1ee33e59b220d572724da1c0209c2e6f14
domivbhh <hariharanvbit123@gmail.com>
2024-04-13 14:28:54 +0530

    auth app modified with estate

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {useSelector} from 'react-redux';
+import {FaSearch} from 'react-icons/fa'
 
 const Header = () => {
   const currentUser=useSelector((state)=>state.user)
   // const{profilePicture}=currentUser.currentUser
   // console.log(username)
   return (
-    <div className="bg-slate-200 ">
+    <header className="bg-slate-200 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold">Auth App</h1>
+        <Link to='/'>
+
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">V B</span>
+          <span className="text-slate-700">-Estate</span>
+        </h1>
+        
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search...."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
         <ul className="flex gap-4">
-          <Link to="/">
-            <li>Home</li>
+          <Link to='/'>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </Link>
           <Link to="/about">
-            <li>About</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </Link>
-          <Link to="/profile">
-            {currentUser.currentUser!==null ? (
-              <img
-                className="h-7 w-7 rounded-full object-cover"
-                src={currentUser?.currentUser?.profilePicture}
-                alt="img"
-              />
-            ) : (
-              <li>Sign in</li>
-            )}
-          </Link>
+          {currentUser.currentUser ? (
+            <Link to="/profile">
+              <li className="hidden sm:inline text-slate-700 hover:underline">
+                Profile
+              </li>
+            </Link>
+          ) : (
+            <Link to="/signin">
+              <li>Sign In</li>
+            </Link>
+          )}
         </ul>
       </div>
-    </div>
+    </header>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import 'homeStyle.css'
 
 const Home = () => {
+  const[todos,setTodos]=useState([])
   return (
     <div className='px-4 py-12 max-w-2xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-700 '>Welcome to my Auth App</h1>
       <p className='mb-4 text-slate-800'>This is a Full stack web Application built with the MERN(MONGODB,EXPRESS,REACT JS,NODE JS) stack.It includes authentication features that allow users to signup ,login ,and logout and provides access to protected routes only for authenticated users</p>
     </div>
-  )
+   
+  );
 }
 
 export default Home
